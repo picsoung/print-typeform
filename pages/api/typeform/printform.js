a70a341c4ec2a1c30a53ff5371f2a6910f2f22e4
Nicolas Grenié <nicolas.grenie@gmail.com>
2024-07-05 12:35:21 +0200

    working print

--- pages/api/typeform/printform.js
+++ pages/api/typeform/printform.js
@@ -3,13 +3,16 @@ import PDFDocument from "pdfkit";
 import blobStream from "blob-stream";
 import axios from "axios";
 import SVGtoPDF from "svg-to-pdfkit";
+import qr from "qr-image";
+import fs from "fs";
+
 export default async (req, res) => {
   // typeform/printform?form_id=abc123
 
   const session = await getSession({ req });
   console.log("session", session);
   if (!session) {
-    res.send({ eror: "Not authorized" });
+    return res.send({ eror: "Not authorized" });
   }
   const { form_id } = req.query;
   const formDef = await fetch(`https://api.typeform.com/forms/${form_id}`, {
@@ -19,7 +22,7 @@ export default async (req, res) => {
   }).then((resp) => resp.json());
 
   if (!formDef) {
-    res.send({ eror: "Error retrieving form" });
+    return res.send({ eror: "Error retrieving form" });
   }
 
   const doc = new PDFDocument();
@@ -35,6 +38,9 @@ export default async (req, res) => {
   doc.fontSize(8).text("Printed by Typeform2PDF", x, doc.page.height - 30, {
     height: doc.page.height,
   });
+  var code = qr.imageSync(formDef._links.display, { type: "png" });
+  doc.image(code, doc.page.width - 100, doc.page.height - 100, { width: 100 });
+
   doc.end();
   res.writeHead(200, {
     "Content-Type": "application/pdf",
@@ -52,7 +58,6 @@ const downloadImage = async (url) => {
   // First request to get headers only
   const headResponse = await axios.get(url);
   const contentType = headResponse.headers["content-type"];
-  console.log(contentType);
   if (contentType === "image/svg+xml") {
     // If SVG, return the data as text
     const response = await axios.get(url);
@@ -60,7 +65,7 @@ const downloadImage = async (url) => {
   } else {
     // For other types, return the data as an ArrayBuffer
     const response = await axios.get(url, { responseType: "arraybuffer" });
-    console.log(response.data);
+    // console.log(response.data);
     return Buffer.from(response.data);
   }
 };
@@ -70,91 +75,160 @@ function indexToLetter(index) {
 }
 
 const resetFont = (doc) => {
-  doc.fillColor("black").fontSize(12);
+  doc.font("Helvetica").fillColor("black").fontSize(12);
+};
+
+const renderField = async (field, doc, index) => {
+  if (
+    field.type === "dropdown" ||
+    field.type === "multiple_choice" ||
+    field.type === "picture_choice" ||
+    field.type === "ranking"
+  ) {
+    if (field.properties.allow_multiple_selection) {
+      let instruction_text = "Pick as many as you want";
+      if (field.properties.validations) {
+        if (
+          field.properties.validations.min_selection &&
+          field.properties.validations.max_selection
+        ) {
+          instruction_text = `Select ${field.properties.validations.min_selection} options`;
+        } else {
+          instruction_text = `Pick between ${field.properties.validations.min_selection} and ${field.properties.validations.max_selection} choices`;
+        }
+      }
+      doc
+        .fillColor("gray")
+        .fontSize(8)
+        .text(instruction_text, { oblique: true });
+    }
+    resetFont(doc);
+
+    for (const [choiceIndex, choice] of field.properties.choices.entries()) {
+      doc.moveDown(0.5);
+      let opt = doc.text(`[ ] (${indexToLetter(choiceIndex)}) ${choice.label}`);
+      if (choice.attachment && choice.attachment.type == "image") {
+        const imageData = await downloadImage(choice.attachment.href);
+        if (typeof imageData === "string") {
+          // If imageData is a string, it's assumed to be SVG data
+          await doc.addSVG(imageData, opt.x, opt.y, {
+            width: 30,
+            preserveAspectRatio: "xMinYMin meet",
+            assumePt: true,
+          });
+          doc.moveDown(1);
+        } else if (Buffer.isBuffer(imageData)) {
+          // If imageData is a Buffer, it's assumed to be an image in a format like PNG or JPEG
+          await doc.image(imageData, opt.x, opt.y, { height: 30 });
+        } else {
+          console.error("Unsupported image data type");
+        }
+        //     y += 40; // Adjust for spacing after the image
+        doc.moveDown(0.5);
+      }
+    }
+    if (field.properties.allow_other_choice) {
+      doc.moveDown(0.5);
+      doc.text(`[ ] Other`);
+      // doc.formText(`field.${field.id}`, 200, 20);
+    }
+  } else if (field.type === "yes_no" || field.type === "legal") {
+    doc.text(`[ ] Yes`);
+    doc.text(`[ ] No`);
+  } else if (field.type === "short_text") {
+    doc.text(`____________________________________________________`);
+  } else if (field.type === "long_text") {
+    doc.text(`____________________________________________________`);
+    doc.text(`____________________________________________________`);
+    doc.text(`____________________________________________________`);
+  } else if (field.type === "opinion_scale" || field.type === "rating") {
+    doc.moveDown(0.5);
+    const optionSpacing = 40; // Space between options
+    let originalX = doc.x;
+    let startX = doc.x;
+    let startY = doc.y;
+    let options = generateStepsText(field.properties);
+    options.forEach((option) => {
+      drawRadioButton(
+        doc,
+        startX,
+        startY,
+        field.properties.shape || "",
+        option
+      );
+      startX += optionSpacing; // Move down for the next button
+    });
+    doc.x = originalX;
+  } else if (field.type === "date") {
+    const structure = field.properties.structure;
+    const separator = field.properties.separator;
+    let date_text = "";
+    switch (structure) {
+      case "DDMMYYYY":
+        date_text = `DD${separator}MM${separator}YYYY`;
+      case "MMDDYYYY":
+        date_text = `MM${separator}DD${separator}YYYY`;
+      case "YYYYMMDD":
+        date_text = `YYYY${separator}MM${separator}DD`;
+    }
+    doc
+      .fillColor("gray")
+      .fontSize(10)
+      .text(`Format: ${date_text}`, { oblique: true });
+    doc.moveDown(0.5);
+    resetFont(doc);
+    doc.text(`${date_text.replace(/[a-zA-Z]/g, "_")}`);
+  } else if (field.type === "address") {
+    for (const [groupIndex, groupField] of field.properties.fields.entries()) {
+      displayQuestionTitle(doc, groupField, index, groupIndex, true);
+      await renderField(groupField, doc);
+    }
+  } else if (field.type != "statement") {
+    doc.text(`____________________________________________________`);
+  }
+  doc.moveDown(1);
+};
+const displayQuestionTitle = (doc, field, index, groupIndex, isGroup) => {
+  let required = field.validations && field.validations.required;
+  let groupIndexTxt = isGroup ? `.${indexToLetter(groupIndex)}` : "";
+  let q_txt = doc
+    .font("Helvetica-Bold")
+    .text(`${index + 1}${groupIndexTxt} - [${field.type}] ${field.title}`, {
+      continued: required,
+    });
+  doc
+    .fillColor("gray")
+    .fontSize(10)
+    .text(field.properties.description, { oblique: true });
+  resetFont(doc);
+  if (required) {
+    doc.fillColor("red").fontSize(12).text(" *", { continued: false });
+    doc.moveDown(0.5);
+    resetFont(doc);
+  }
 };
 
 const processFields = async (fields, doc, x, y) => {
   for (const [index, field] of fields.entries()) {
     doc.moveDown(1); // Adjust for spacing after the question text
-    let required = field.validations && field.validations.required;
-    let q_txt = doc
-      .font("Helvetica-Bold")
-      .text(`${index + 1} - [${field.type}] ${field.title}`, {
-        continued: required,
-      });
-    doc.font("Helvetica");
-    if (required) {
-      doc.fillColor("red").fontSize(12).text(" *", { continued: false });
-      doc.moveDown(0.5);
-      resetFont(doc);
-    }
+    displayQuestionTitle(doc, field, index);
 
-    if (field.type === "group") {
-      // Handle group type if necessary
-    } else if (
-      field.type === "dropdown" ||
-      field.type === "multiple_choice" ||
-      field.type === "picture_choice"
+    if (
+      field.type === "group" ||
+      field.type === "contact_info" ||
+      field.type === "matrix" ||
+      field.type === "address"
     ) {
-      if (field.properties.allow_multiple_selection) {
-        let instruction_text = "Pick as many as you want";
-        if (field.properties.validations) {
-          if (
-            field.properties.validations.min_selection &&
-            field.properties.validations.max_selection
-          ) {
-            instruction_text = `Select ${field.properties.validations.min_selection} options`;
-          } else {
-            instruction_text = `Pick between ${field.properties.validations.min_selection} and ${field.properties.validations.max_selection} choices`;
-          }
-        }
-        doc
-          .fillColor("gray")
-          .fontSize(8)
-          .text(instruction_text, { oblique: true });
+      // Handle group type if necessary
+      for (const [
+        groupIndex,
+        groupField,
+      ] of field.properties.fields.entries()) {
+        displayQuestionTitle(doc, groupField, index, groupIndex, true);
+        await renderField(groupField, doc, index);
       }
-      resetFont(doc);
-
-      for (const [choiceIndex, choice] of field.properties.choices.entries()) {
-        doc.moveDown(0.5);
-        let opt = doc.text(`[ ] (${indexToLetter(choiceIndex)}) ${choice.label}`);
-        console.log(choice.label, choice.attachment, opt.x, opt.y);
-        if (choice.attachment && choice.attachment.type == "image") {
-          const imageData = await downloadImage(choice.attachment.href);
-          if (typeof imageData === "string") {
-            // If imageData is a string, it's assumed to be SVG data
-            await doc.addSVG(imageData, opt.x, opt.y, { width: 30 , preserveAspectRatio: "xMinYMin meet", assumePt: true});
-            doc.moveDown(1);
-          } else if (Buffer.isBuffer(imageData)) {
-            // If imageData is a Buffer, it's assumed to be an image in a format like PNG or JPEG
-            await doc.image(imageData, opt.x, opt.y,  { height: 30 });
-          } else {
-            console.error("Unsupported image data type");
-          }
-          //     y += 40; // Adjust for spacing after the image
-          doc.moveDown(0.5);
-        }
-      }
-      if (field.properties.allow_other_choice) {
-        doc.moveDown(0.5);
-        doc.text(`[ ] Other`);
-        // doc.formText(`field.${field.id}`, 200, 20);
-      }
-    } else if (field.type === "yes_no" || field.type === "legal") {
-      doc.text(`[ ] Yes`);
-      doc.text(`[ ] No`);
-    } else if (field.type === "short_text") {
-      doc.text(`____________________________________________________`);
-    } else if (field.type === "long_text") {
-      doc.text(`____________________________________________________`);
-      doc.text(`____________________________________________________`);
-      doc.text(`____________________________________________________`);
-    } else if (field.type != "statement") {
-      // doc.text(q_txt);
-      let txt = doc.text(`____________________________________________________`);
-      console.log(txt.x, txt.y);
-      // doc.formText(`field.${field.id}`, q_txt.x,300, 200, 20);
-      doc.moveDown(0.5);
+    } else {
+      await renderField(field, doc, index);
     }
   }
 };
@@ -162,3 +236,31 @@ const processFields = async (fields, doc, x, y) => {
 PDFDocument.prototype.addSVG = function (svg, x, y, options) {
   return SVGtoPDF(this, svg, x, y, options), this;
 };
+
+function drawRadioButton(doc, x, y, shape, text) {
+  if (shape) {
+    const readFileAsync = fs.readFileSync(`public/shapes/${shape}.svg`, "utf8");
+    doc.addSVG(readFileAsync, x, y, {
+      width: 30,
+      preserveAspectRatio: "xMinYMin meet",
+      assumePt: true,
+    });
+  } else {
+    doc.circle(x, y, 5).stroke(); // Draw the circle
+    doc.text(text, x + 10, y - 5); // Draw the text label
+  }
+  doc.moveDown(0.5);
+}
+
+function generateStepsText(properties) {
+  let stepsArray = [];
+  let startAt = properties.start_at_one ? 1 : 0;
+  let endAt = startAt + properties.steps - 1; // Adjust for inclusive range
+
+  for (let i = startAt; i <= endAt; i++) {
+    let stepText = `${i}`; // Customize this text as needed
+    stepsArray.push(stepText);
+  }
+
+  return stepsArray;
+}
